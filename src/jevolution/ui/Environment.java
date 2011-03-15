537b6be33751cd6d5ad52309049f75ad5ff6ba5d
evangates <evan@thoughtmerge.net>
2011-03-15 10:16:10 -0700

    Pulled application logic code out of EnvironmentPanel and into Environment.  The refactoring is not complete.

---
+++ src/jevolution/ui/Environment.java
@@ -0,0 +1,156 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jevolution.ui;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import jevolution.Creature;
+import jevolution.Thing;
+import jevolution.expressions.CreatureExpression;
+
+/**
+ *
+ * @author kuhlmancer
+ */
+public class Environment {
+	final static int minNumCreatures = 20;
+	final static int preferredNumCreatures = 75;
+	final static int maxNumCreatures = 100;
+
+	private CreatureExpression strengthExpression;
+	private CreatureExpression costOfLivingExpression;
+
+	private int width, height;
+	private List<Creature> creatures;
+
+	/*
+	 * This should be adjustable through the UI.  Don't assume that it won't change.
+	 */
+	private double timeBetweenRandoms = 1;
+
+	/*
+	 * This should be adjustable through the UI.  Don't assume that it won't change.
+	 */
+	private double timeBetweenMatings = 0.05;
+
+	private double timeSinceLastRandom;
+	private double timeSinceLastMating;
+
+	// TODO: this is here as part of an artifact of factoring Environment out of EnvironmentPanel
+	// the dependency should go to other way when done refactoring
+	private EnvironmentPanel panel;
+
+	/*
+	 * TODO: panel should not be needed when done factoring out Environment.
+	 * An Environment instance should be injected into the EnvironmentPanel instead.
+	 */
+	public Environment(int width, int height, EnvironmentPanel panel) {
+		// TODO: remove this when the panel references is no longer needed
+		this.panel = panel;
+
+		this.width = width;
+		this.height = height;
+
+		timeSinceLastRandom = 0;
+		timeSinceLastMating = 0;
+
+		creatures = new LinkedList<Creature>();
+
+		addThings();
+	}
+
+	private void addThings() {
+		for(int i = 0; i < preferredNumCreatures; ++i) {
+			creatures.add(Creature.random(panel));
+		}
+	}
+
+	/*
+	 * Recheck if this is needed after factoring out Environment from EnvironmentPanel.
+	 */
+	List<Creature> getCreatures() {
+		return creatures;
+	}
+
+	public CreatureExpression getStrengthExpression() {
+		return this.strengthExpression;
+	}
+
+	public CreatureExpression getCostOfLivingExpression() {
+		return this.costOfLivingExpression;
+	}
+
+	public void setStrengthExpression(CreatureExpression expr) {
+		this.strengthExpression = expr;
+	}
+
+	public void setCostOfLivingExpression(CreatureExpression expr) {
+		this.costOfLivingExpression = expr;
+	}
+
+	public void setMatingsPerSecond(double matingsPerSecond) {
+		timeBetweenMatings = 1d/matingsPerSecond;
+	}
+
+	public void setRandomCreaturesPerSecond(double creaturesPerSecond) {
+		timeBetweenRandoms = 1d/creaturesPerSecond;
+	}
+
+	public void tick(double timePerFrame) {
+		for(Thing t: creatures) {
+			t.tick(timePerFrame);
+		}
+
+		LinkedList<Thing> deads = new LinkedList<Thing>();
+
+		for(Thing t: creatures) {
+			for (Thing other: creatures) {
+				if (t != other) {
+					if (t.getShape().intersects(other.getShape().getBounds2D())) {
+						t.interactWith(other, timePerFrame);
+					}
+				}
+			}
+		}
+
+		for(Thing t: creatures) {
+			if (t.isDead()) {
+				deads.add(t);
+			}
+			else {
+				t.clipEnergy();
+			}
+		}
+
+		creatures.removeAll(deads);
+
+		if (timeSinceLastRandom > timeBetweenRandoms) {
+			creatures.add(Creature.random(panel));
+			timeSinceLastRandom = 0;
+		}
+
+		if (timeSinceLastMating > timeBetweenMatings && creatures.size() > 0) {
+			Random r = new Random();
+
+			int index1 = r.nextInt(creatures.size());
+			int index2 = r.nextInt(creatures.size());
+
+			Creature one = creatures.get(index1);
+			Creature two = creatures.get(index2);
+
+			Creature child = Creature.mate(one, two);
+
+			if (child != null) {
+				creatures.add(child);
+				timeSinceLastMating = 0;
+			}
+		}
+
+		timeSinceLastRandom += timePerFrame;
+		timeSinceLastMating += timePerFrame;
+	}
+}
--- src/jevolution/ui/EnvironmentPanel.java
+++ src/jevolution/ui/EnvironmentPanel.java
@@ -4,84 +4,29 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.util.LinkedList;
-import java.util.Random;
 
 import javax.swing.JPanel;
-import jevolution.Creature;
 import jevolution.Thing;
 import jevolution.expressions.CreatureExpression;
 
 
 public class EnvironmentPanel extends JPanel {
+	// TODO: move to Creature.random
+	// that's the only place it's used
 	public final static double maxVelocity = 400;
 
-	private CreatureExpression strengthExpression;
-	private CreatureExpression costOfLivingExpression;
-	
-	final static int minNumCreatures = 20;
-	final static int preferredNumCreatures = 75;
-	final static int maxNumCreatures = 100;
-
-	private double timeBetweenRandoms = 1;
-	private double timeBetweenMatings = 0.05;
-	
 	private static final long serialVersionUID = 1L;
-	private LinkedList<Creature> creatures;
 
-	private int height;
-	private int width;
-
-	private double timeSinceLastRandom;
-	private double timeSinceLastMating;
+	private Environment environment;
 	
 	public EnvironmentPanel(int width, int height) {
-		this.width = width;
-		this.height = height;
-		
 		Dimension size = new Dimension(width,height);
 		setSize(size);
 		setPreferredSize(size);
 		
 		setBackground(Color.DARK_GRAY);
 		
-		creatures = new LinkedList<Creature>();
-		
-		addThings();
-		
-		timeSinceLastRandom = 0;
-	}
-	
-	private void addThings() {
-		for(int i = 0; i < preferredNumCreatures; ++i) {
-			creatures.add(Creature.random(this));
-		}
-	}
-
-	public CreatureExpression getStrengthExpression() {
-		return this.strengthExpression;
-	}
-
-	public CreatureExpression getCostOfLivingExpression() {
-		return this.costOfLivingExpression;
-	}
-
-	public void setStrengthExpression(CreatureExpression expr) {
-		this.strengthExpression = expr;
-//		System.out.println(String.format("evironment panel strength function updated: %s", expr.toString()));
-	}
-
-	public void setCostOfLivingExpression(CreatureExpression expr) {
-		this.costOfLivingExpression = expr;
-//		System.out.println(String.format("environment panel cost of living function updated: %s", expr.toString()));
-	}
-
-	public void setMatingsPerSecond(double matingsPerSecond) {
-		timeBetweenMatings = 1d/matingsPerSecond;
-	}
-
-	public void setRandomCreaturesPerSecond(double creaturesPerSecond) {
-		timeBetweenRandoms = 1d/creaturesPerSecond;
+		environment = new Environment(width, height, this);
 	}
 	
 	@Override
@@ -90,62 +35,36 @@ public class EnvironmentPanel extends JPanel {
 
 		Graphics2D g = (Graphics2D) gc;
 
-		for(Thing t : creatures) {
+		for(Thing t : environment.getCreatures()) {
 			t.draw(g);
 		}
 	}
-	
+
 	public void tick(double timePerFrame) {
-		for(Thing t: creatures) {
-			t.tick(timePerFrame);
-		}
-		
-		LinkedList<Thing> deads = new LinkedList<Thing>();
-		
-		for(Thing t: creatures) {
-			for (Thing other: creatures) {
-				if (t != other) {
-					if (t.getShape().intersects(other.getShape().getBounds2D())) {
-						t.interactWith(other, timePerFrame);
-					}
-				}
-			}
-		}
-		
-		for(Thing t: creatures) {
-			if (t.isDead()) {
-				deads.add(t);
-			}
-			else {
-				t.clipEnergy();
-			}
-		}
-		
-		creatures.removeAll(deads);
-		
-		if (timeSinceLastRandom > timeBetweenRandoms) {
-			creatures.add(Creature.random(this));
-			timeSinceLastRandom = 0;
-		}
-		
-		if (timeSinceLastMating > timeBetweenMatings && creatures.size() > 0) {
-			Random r = new Random();
-			
-			int index1 = r.nextInt(creatures.size());
-			int index2 = r.nextInt(creatures.size());
-			
-			Creature one = creatures.get(index1);
-			Creature two = creatures.get(index2);
-			
-			Creature child = Creature.mate(one, two);
-	
-			if (child != null) {
-				creatures.add(child);
-				timeSinceLastMating = 0;
-			}
-		}
-		
-		timeSinceLastRandom += timePerFrame;
-		timeSinceLastMating += timePerFrame;
+		environment.tick(timePerFrame);
+	}
+
+	public CreatureExpression getStrengthExpression() {
+		return environment.getStrengthExpression();
+	}
+
+	public CreatureExpression getCostOfLivingExpression() {
+		return environment.getCostOfLivingExpression();
+	}
+
+	public void setStrengthExpression(CreatureExpression expr) {
+		environment.setStrengthExpression(expr);
+	}
+
+	public void setCostOfLivingExpression(CreatureExpression expr) {
+		environment.setCostOfLivingExpression(expr);
+	}
+
+	public void setMatingsPerSecond(double matingsPerSecond) {
+		environment.setMatingsPerSecond(1d/matingsPerSecond);
+	}
+
+	public void setRandomCreaturesPerSecond(double creaturesPerSecond) {
+		environment.setRandomCreaturesPerSecond(1d/creaturesPerSecond);
 	}
 }
